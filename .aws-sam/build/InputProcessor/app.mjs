26d589efd7834188bee33f91000bcbbcd1eb14ef
Bernardo Di Miceli <bdm.444@gmail.com>
2023-08-23 17:41:14 +0200

    aggiunta funzionalità di lettura da s3 alla lambda (che però non funziona)

--- .aws-sam/build/InputProcessor/app.mjs
+++ .aws-sam/build/InputProcessor/app.mjs
@@ -1,4 +1,21 @@
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+
+const client = new S3Client({});
+
 export const handler = async (event) => {
-    console.log(`Un file è stato caricato in InputBucket`);
-    return true;
+    const uploadInfo = event.Records[0].s3;
+    console.log(uploadInfo);
+
+    const params = {
+        Bucket: uploadInfo.bucket.name,
+        Key: uploadInfo.object.key
+    };
+    console.log(params);
+
+    const command = new GetObjectCommand(params);
+    const response = await client.send(command);
+
+    console.log(response);
+
+    return response;
 }

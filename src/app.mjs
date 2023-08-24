a51206c56e46c35f3555f637458eb53c5943ec17
Bernardo Di Miceli <bdm.444@gmail.com>
2023-08-24 15:54:07 +0200

    aggiunta la logica per effettuare una semplice select sul csv caricato sul primo bucket

--- src/app.mjs
+++ src/app.mjs
@@ -1,5 +1,4 @@
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
-import { createWriteStream } from "fs";
+import { S3Client, GetObjectCommand, SelectObjectContentCommand } from "@aws-sdk/client-s3";
 
 const client = new S3Client({});
 
@@ -7,33 +6,59 @@ export const handler = async (event) => {
     const uploadInfo = event.Records[0].s3;
     console.log(uploadInfo);
 
+    // const params = {
+    //     "Bucket": uploadInfo.bucket.name,
+    //     "Key": uploadInfo.object.key
+    // };
     const params = {
-        "Bucket": uploadInfo.bucket.name,
-        "Key": uploadInfo.object.key
+        Bucket: uploadInfo.bucket.name,
+        Key: uploadInfo.object.key,
+        Expression: "SELECT Nome FROM S3Object",
+        ExpressionType: "SQL",
+        InputSerialization: {
+            CSV: {
+                FileHeaderInfo: 'USE',
+            },
+            CompressionType: 'NONE',
+        },
+        OutputSerialization: {
+            CSV: {
+                RecordDelimiter: '\n',
+                FieldDelimiter: ','
+            }
+        }
     };
     console.log(params);
+    let dataChunks = [];
+    const textDecoder = new TextDecoder();
 
-    const command = new GetObjectCommand(params);
-    const response = await client.send(command);
-    const stream = response.Body;
-    let fileContent = '';
-    stream.on('data', (chunk) => {
-        fileContent += chunk.toString('utf-8');
-    });
-    stream.on('end', () => {
-        console.log("File content: ", fileContent);
-    });
+    // const command = new GetObjectCommand(params);
+    // const response = await client.send(command);
+    // const stream = response.Body;
+    // let fileContent = '';
+    // stream.on('data', (chunk) => {
+    //     fileContent += chunk.toString('utf-8');
+    // });
+    // stream.on('end', () => {
+    //     console.log("File content: ", fileContent);
+    // });
 
-    // const { response } = await client.send(command);
-    // const responseBody = await streamToString(response);
-    // console.log(responseBody);
+    try {
+        const command = new SelectObjectContentCommand(params);
+        const response = await client.send(command);
+        console.log(response);
+
+        for await (const eventChunk of response.Payload) {
+            if (eventChunk.Records) {
+                const chunkData = textDecoder.decode(eventChunk.Records.Payload);
+                dataChunks.push(chunkData);
+            }
+        }
+        const selectedNames = dataChunks.join("");
+        console.log(selectedNames);
+    } catch (error) {
+        console.error("Error: ", error);
+    }
     
     return true;
 }
-
-// const streamToString = (stream) => new Promise((resolve, reject) => {
-//     const chunks = [];
-//     stream.on("data", (chunk) => chunks.push(chunk));
-//     stream.on("error", reject);
-//     stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-// })

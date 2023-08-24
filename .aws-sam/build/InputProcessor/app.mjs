25ba0d04d55c383f9e4d264eca3ee2e9adf18366
Bernardo Di Miceli <bdm.444@gmail.com>
2023-08-24 14:33:23 +0200

    aggiunta logica per la gestione dello stream restituito da getobjectcommand nella lambda

--- .aws-sam/build/InputProcessor/app.mjs
+++ .aws-sam/build/InputProcessor/app.mjs
@@ -1,4 +1,5 @@
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { createWriteStream } from "fs";
 
 const client = new S3Client({});
 
@@ -7,15 +8,32 @@ export const handler = async (event) => {
     console.log(uploadInfo);
 
     const params = {
-        Bucket: uploadInfo.bucket.name,
-        Key: uploadInfo.object.key
+        "Bucket": uploadInfo.bucket.name,
+        "Key": uploadInfo.object.key
     };
     console.log(params);
 
     const command = new GetObjectCommand(params);
     const response = await client.send(command);
+    const stream = response.Body;
+    let fileContent = '';
+    stream.on('data', (chunk) => {
+        fileContent += chunk.toString('utf-8');
+    });
+    stream.on('end', () => {
+        console.log("File content: ", fileContent);
+    });
 
-    console.log(response);
-
-    return response;
+    // const { response } = await client.send(command);
+    // const responseBody = await streamToString(response);
+    // console.log(responseBody);
+    
+    return true;
 }
+
+// const streamToString = (stream) => new Promise((resolve, reject) => {
+//     const chunks = [];
+//     stream.on("data", (chunk) => chunks.push(chunk));
+//     stream.on("error", reject);
+//     stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+// })
